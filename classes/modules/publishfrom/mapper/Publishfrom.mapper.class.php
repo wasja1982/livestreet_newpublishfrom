ab70fb6afca1075ead3656f9e04722fab719b08d
wasja1982 <wasja@yandex.ru>
2013-10-20 16:47:35 +0300

    Улучшено формирование SQL-запроса.

--- classes/modules/publishfrom/mapper/Publishfrom.mapper.class.php
+++ classes/modules/publishfrom/mapper/Publishfrom.mapper.class.php
@@ -1,12 +1,15 @@
 <?php
+function addcslash($str) {
+	return addcslashes($str,"'");
+}
+
 class PluginPublishfrom_ModulePublishfrom_MapperPublishfrom extends Mapper {
 	public function getUserList(){
 		$users = Config::Get('plugin.publishfrom.user_logins');
 		$ids = Config::Get('plugin.publishfrom.user_ids');
 		$expr = Config::Get('plugin.publishfrom.user_id_expression');
-		if(is_array($users)){
-			foreach($users as $i=>$u)
-				$logins_in .= ($i == 0?'':',')."'$u'";
+		if(is_array($users)){			$users = array_unique(array_map("addcslash", $users));
+			$logins_in = '\''.implode("','", $users).'\'';
 		}
 		if($expr){
 			$expr = explode("-",$expr);
@@ -14,10 +17,10 @@ class PluginPublishfrom_ModulePublishfrom_MapperPublishfrom extends Mapper {
 			while($s <= $e) {
 				$ids[] = $s++;
 			}
-		}		
+		}
 		if(is_array($ids)){
-			foreach($ids as $i=>$u)
-				$ids_in .= ($i == 0?'':',').$u;
+			$ids = array_unique(array_map("intval", $ids), SORT_NUMERIC);
+			$ids_in = implode(",", $ids);
 		}
 		if($logins_in||$ids_in){
 			$or = ($logins_in&&$ids_in?' OR ':'');
@@ -26,19 +29,19 @@ class PluginPublishfrom_ModulePublishfrom_MapperPublishfrom extends Mapper {
 		}
 		return array();
 	}
-	
-	public function UpdateTopic($oTopic) {		
-		$sql = "UPDATE ".Config::Get('db.table.topic')." 
-			SET 
+
+	public function UpdateTopic($oTopic) {
+		$sql = "UPDATE ".Config::Get('db.table.topic')."
+			SET
 				user_id= ?d,
 				blog_id= ?d
 			WHERE
 				topic_id = ?d
-		";			
+		";
 		if ($this->oDb->query($sql,$oTopic->getUserId(),$oTopic->getBlogId(),$oTopic->getId())) {
 			//$this->UpdateTopicContent($oTopic);
 			return true;
-		}		
+		}
 		return false;
-	}	
+	}
 }
